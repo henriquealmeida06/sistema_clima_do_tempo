a5fbb7a87045b3999bf6e8248a8b54087bec8448
Henrique Almeida <marceloh500@gmail.com>
2023-10-10 00:17:01 -0300

    Incluindo API

--- src/pages/PrevisaoDoTempo.js
+++ src/pages/PrevisaoDoTempo.js
@@ -1,12 +1,41 @@
 import styles from './PrevisaoDoTempo.module.css'
 import LogoSol from './../assets/img/img_sol.jpg'
+import {useState} from 'react'
 
 function PrevisaoDoTempo(){
+
+    const [inputCidade, setInputCidade] = useState('')
+    const [cidade, setCidade] = useState('')
+    const [dadosClima, setDadosClima] = useState()
+    const [tempMin, setTempMin] = useState(0)
+    const [tempMax, setTempMax] = useState(0)
+    const chaveApi = '848cc25d52bfcd91afbb3cd16913a623'
+
+
+    const buscarDados = async ()=>{
+        try{
+            const resposta = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${inputCidade}&appid=${chaveApi}`
+            );
+            const dados = await resposta.json();
+
+            setDadosClima(dados);
+            
+            
+
+            setTempMax((dadosClima.main.temp_max-32)*5/9)
+        }catch(erro){
+            console.error('Houve um erro na obtenção dos dados da previsão')
+        }
+        setCidade(inputCidade)
+    }
+
+
     return(
         <div className={styles.containerPrincipal}>
             <div className={styles.inputButton}>
-            <input type="text" placeholder="Digite o nome da cidade"/> 
-            <button>Buscar</button>
+            <input type="text" placeholder="Digite o nome da cidade" value={inputCidade} onChange={(e)=> setInputCidade(e.target.value)}/> 
+            <button onClick={buscarDados}>Buscar</button>
             </div>
             <div className={styles.divDiaAtual}>
                 <p>Segunda-Feira</p>
@@ -14,7 +43,7 @@ function PrevisaoDoTempo(){
             </div>
             <div className={styles.containerSecundario}>
                 <div className={styles.divCidade}>
-                    <p>Arapiraca</p>
+                    <p>{cidade}</p>
 
                 </div>
                 <div className={styles.divSimboloClima}>
@@ -25,11 +54,11 @@ function PrevisaoDoTempo(){
                 <div className={styles.divMaxMin}>
                     <div>
                     <p className={styles.fontePadrao}>mínima</p>
-                    <p className={styles.fonteNumero}>24°</p>
+                    <p className={styles.fonteNumero}>{tempMin}</p>
                     </div>
                     <div>
                     <p className={styles.fontePadrao}>máxima</p>
-                    <p className={styles.fonteNumero}>28°</p>
+                    <p className={styles.fonteNumero}>{tempMax}</p>
                     </div>
 
                 </div>
